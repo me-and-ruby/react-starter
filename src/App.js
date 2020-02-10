a8bc063651e9ac25f1a6dc7b38c876484e7a85d9
Joash Nyabuti <nyabutijoash@yahoo.com>
2020-02-09 19:24:20 -0600

    added shared folder to hold dish data and pass it down to child compo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import Menu from "./components/Menu"
 import {Navbar, NavbarBrand} from "reactstrap";
+import { DISHES } from './shared/dish';
 
 
 
-function App() {
-  return (
-    <div className="App">
 
-      <Navbar dark color="primary" >
-        <div className="container">
-          <NavbarBrand href="/">MyBrand</NavbarBrand>
-        </div>
+class App extends React.Component{
+     constructor(props){
+       super(props);
+       this.state={
+         dishes: DISHES
+       }
+     }
+  render(){
 
-      </Navbar>
-      <Menu/>
-      
-    </div>
-  );
+    return (
+      <div className="App">
+  
+        <Navbar dark color="primary" >
+          <div className="container">
+            <NavbarBrand href="/">MyBrand</NavbarBrand>
+          </div>
+  
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
